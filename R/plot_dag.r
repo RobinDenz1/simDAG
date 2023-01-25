5804440071bca3c709d7b967e84cf4a72a2abb42
Robin Denz <robindenz@web.de>
2023-01-25 10:43:40 +0100

    feat: add small plot_dag function

---
+++ R/plot_dag.r
@@ -0,0 +1,23 @@
+
+## a quick and dirty way to plot the DAG using the node lists
+#' @export
+plot_dag <- function(root_nodes, child_nodes) {
+
+  requireNamespace("ggplot2")
+  requireNamespace("igraph")
+  requireNamespace("ggraph")
+
+  # obtain adjacency matrix
+  adj_mat <- nodes2adjacency_mat(root_nodes=root_nodes, child_nodes=child_nodes)
+
+  # create graph object from it
+  adj_graph <- igraph::graph.adjacency(adjmatrix=adj_mat, mode="directed")
+
+  # plot it
+  ggraph::ggraph(adj_graph, layout="graphopt") +
+    ggraph::geom_edge_link(ggplot2::aes(
+      start_cap=ggraph::label_rect(.data$node1.name),
+      end_cap=ggraph::label_rect(.data$node2.name)),
+      arrow=ggplot2::arrow(length=unit(4, "mm"))) +
+    ggraph::geom_node_label(aes(label=.data$name))
+}
