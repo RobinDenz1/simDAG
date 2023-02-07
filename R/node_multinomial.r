ecb6b32de8c45f4a29b891815565649e45a66c14
Robin Enrico Denz <denz@amib.ruhr-uni-bochum.de>
2023-02-07 11:02:47 +0100

    feat: add negative binomial regression node fix: node_multinomial errors somewhat fixed

--- R/node_multinomial.r
+++ R/node_multinomial.r
@@ -5,13 +5,16 @@
 # TODO: this doesn't seem to work at all. Also, the intercepts are missing
 #       needs some major reworks
 #' @export
-node_multinomial <- function(data, parents, betas, class_labels=NULL,
-                             return_prob=FALSE) {
+node_multinomial <- function(data, parents, betas, intercepts,
+                             class_labels=NULL, return_prob=FALSE) {
   # prep data
-  mat <- as.matrix(data[, parents, with=FALSE])
+  mat <- as.matrix(cbind(rep(1, nrow(data)), data[, parents, with=FALSE]))
+
+  # add intercepts to betas
+  betas <- cbind(intercepts, betas)
 
   # generate scores and sample from those
-  probs <- cbind(apply(betas, MARGIN=1, FUN=function(x, mat){mat %*% x},
+  probs <- cbind(apply(betas, MARGIN=1, FUN=function(x, mat){exp(mat %*% x)},
                        mat=mat))
   choice_mat <- t(apply(probs, 1, stats::rmultinom, n=1, size=1))
   out <- apply(choice_mat, 1, function(x) which(x==1))
---
+++ R/node_negative_binomial.r
@@ -0,0 +1,12 @@
+
+## generate data from a negative binomial regression
+#' @export
+node_negative_binomial <- function(data, parents, betas, theta) {
+
+  eta <- as.matrix(data[, parents, with=FALSE]) %*% beta
+
+  out <- stats::rnbinom(n=length(eta), mu=exp(eta), size=theta)
+
+  return(out)
+}
+
